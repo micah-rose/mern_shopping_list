62766cf165b1b7fadc86ab0c4a12d83336c19495
Micah Lund <micah.r.lund@gmail.com>
2020-06-27 19:16:45 -0600

    Removed comments on server.js file.

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-// const express = require("express"); //back end framework
-// const mongoose = require("mongoose"); //ORM to interact with MongoDB
-// const bodyParser = require("body-parser"); //take requests and get data from the body - name of POST from POST request
+const express = require("express"); //back end framework
+const mongoose = require("mongoose"); //ORM to interact with MongoDB
+const bodyParser = require("body-parser"); //take requests and get data from the body - name of POST from POST request
 
-// const items = require("./routes/api/items");
+const items = require("./routes/api/items");
 
-// const app = express();
+const app = express();
 
-// //Bodyparser Middleware
-// app.use(bodyParser.json());
+//Bodyparser Middleware
+app.use(bodyParser.json());
 
-// //DB Config
-// const db = require("./config/keys").mongoURI;
+//DB Config
+const db = require("./config/keys").mongoURI;
 
-// //Connect to Mongo
-// mongoose
-//   .connect(db, { useNewUrlParser: true })
-//   .then(() => console.log("MongoDB Connected..."))
-//   .catch(err => console.log(err));
+//Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 
-// //Use Routes
-// app.use("/api/items", items);
+//Use Routes
+app.use("/api/items", items);
 
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-// app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
